d0939497e4b53ff4ce77118088bd9d5995e5ce4a
YuriOlivs <yuri.os.2004@outlook.com>
2023-05-06 22:20:26 -0300

    adaptando api para o projeto

--- site/src/controllers/jogadorController.js
+++ site/src/controllers/jogadorController.js
@@ -26,12 +26,48 @@ function listar(req, res) {
 
 function cadastrar(req, res) {
    var nome = req.body.nomeServer;
+   var pontos = req.body.pontosServer;
+   var assists = req.body.assistsServer;
+   var rebotes = req.body.rebotesServer;
+   var tocos = req.body.tocosServer;
+   var arrmsErrados = req.body.arrmsErradosServer;
+   var arrmsCertos = req.body.arrmsCertosServer;
+   var llCertos = req.body.llCertosServer;
+   var llErrados = req.body.llErradosServer;
+   var minutos = req.body.minutosServer;
+   var foulCometidas = req.body.foulCometidasServer;
+   var foulSofridas = req.body.foulSofridasServer;
+   var roubosBola = req.body.roubosBolaServer;
 
-   if(nome == undefined) {
-      res.status(4000).send("Seu nome está undefined!");
+   if (nome === undefined) {
+      return res.status(400).send("O nome está undefined!");
+   } else if (pontos === undefined) {
+      return res.status(400).send("Os pontos estão undefined!");
+   } else if (assists === undefined) {
+      return res.status(400).send("Os assists estão undefined!");
+   } else if (rebotes === undefined) {
+      return res.status(400).send("Os rebotes estão undefined!");
+   } else if (tocos === undefined) {
+      return res.status(400).send("Os tocos estão undefined!");
+   } else if (llErrados === undefined) {
+      return res.status(400).send("Os lances livres errados estão undefined!");
+   } else if (llCertos === undefined) {
+      return res.status(400).send("Os lances livres certos estão undefined!");
+   } else if (arrmsErrados === undefined) {
+      return res.status(400).send("Os arremessos errados estão undefined!");
+   } else if (arrmsCertos === undefined) {
+      return res.status(400).send("Os arremessos certos estão undefined!");
+   } else if (foulCometidas === undefined) {
+      return res.status(400).send("As faltas cometidas estão undefined!");
+   } else if (foulSofridas === undefined) {
+      return res.status(400).send("As faltas sofridas estão undefined!");
+   } else if (minutos === undefined) {
+      return res.status(400).send("Os minutos estão undefined!");
+   } else if (roubosBola === undefined) {
+      return res.status(400).send("Os roubos de bola estão undefined!");
    }
 
-   jogadorModel.cadastrar(nome)
+   jogadorModel.cadastrar(nome, pontos, assists, rebotes, tocos, llErrados, llCertos, arrmsErrados, arrmsCertos, foulCometidas, foulSofridas, minutos, roubosBola)
       .then(
          function(resultado) {
             res.json(resultado);
@@ -40,7 +76,7 @@ function cadastrar(req, res) {
          function (erro) {
             console.log(erro);
             console.log(
-               "\nHouve um erro ao realizar o cadastro1 Erro: ", erro.sqlMessage
+               "\nHouve um erro ao realizar o cadastro Erro: ", erro.sqlMessage
             );
          }
       )
--- site/src/models/jogadorModel.js
+++ site/src/models/jogadorModel.js
@@ -3,17 +3,18 @@ var database  = require("../database/config");
 function listar() {
    console.log("ACESSEI O JOGADOR MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
    var instrucao = `
-      SELECT * FROM jogador;
+      SELECT * FROM jogador ORDER BY dataStat;
    `;
    console.log("Executando a instrução SQL: \n" + instrucao);
    return database.executar(instrucao);
 }
 
-function cadastrar(nome) {
-   console.log("ACESSEI O JOGADOR MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome);
+function cadastrar(nome, pontos, assists, rebotes, tocos, arrmsErrados, arrmsCertos, llCertos, llErrados, minutos, foulCometidas, foulSofridas, roubosBola) {
+   console.log("ACESSEI O JOGADOR MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, pontos, assists, rebotes, tocos, arrmsErrados, arrmsCertos, llCertos, llErrados, minutos, foulCometidas, foulSofridas, roubosBola);
 
    var instrucao = `
-      INSERT INTO jogador (nome) VALUES ('${nome}');
+      INSERT INTO stats (pontos, assists, rebotes, tocos, arrmsErrados, arrmsCertos, lancesLivresCertos, lancesLivresErrados, minutosJogados, faltasCometidas, faltasSofridas, roubosBola, dataStat, fkJogador) 
+      VALUES (${pontos}, ${assists}, ${rebotes}, ${tocos}, ${arrmsErrados}, ${arrmsCertos}, ${llCertos}, ${llErrados}, ${minutos}, ${foulCometidas}, ${foulSofridas}, ${roubosBola}, CURDATE(), (SELECT idJogador FROM jogador WHERE jogador.nome = '${nome}'));
    `;
    console.log("Executando a instrução SQL: \n" + instrucao);
    return database.executar(instrucao);
