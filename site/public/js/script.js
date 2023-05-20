6b1f55554e4ee8b8c1594ac3a5e4e512b475f671
YuriOlivs <yuri.os.2004@outlook.com>
2023-05-20 13:05:44 -0300

    adicionando modal de sucesso ao cadastrar

--- site/public/js/script.js
+++ site/public/js/script.js
@@ -1,57 +1,5 @@
 var jogador = {};
 
-var statsJogador = {};
-
-function CadStatsJogdr() {
-    jogador = {
-        usuario: false,
-        nome: ipt_nome_jogador.value
-    };
-
-    statsJogador = {
-        jogador: jogador,
-        pontos: ipt_pontos.value,
-        rebotes: ipt_rebotes.value,
-        assistencias: ipt_assists.value,
-        tocos: ipt_tocos.value,
-        lancesLivresErrados: Number(ipt_ll_tentados.value - ipt_ll_certos.value),
-        lancesLivresCertos:ipt_ll_certos.value,
-        arremessosErrados: Number(ipt_arrms_tentados.value - ipt_arrms_certos.value),
-        arremessosCertos: ipt_arrms_certos.value,
-        minutosJogador: ipt_minutos.value
-    };
-};
-
-function CadStatsTime() {
-    statsJogador = {
-        jogador: null,
-        pontos: ipt_pontos.value,
-        rebotes: ipt_rebotes.value,
-        assistencias: ipt_assists.value,
-        tocos: ipt_tocos.value,
-        lancesLivresErrados: Number(ipt_ll_tentados.value - ipt_ll_certos.value),
-        lancesLivresCertos:ipt_ll_certos.value,
-        arremessosErrados: Number(ipt_arrms_tentados.value - ipt_arrms_certos.value),
-        arremessosCertos: ipt_arrms_certos.value,
-        minutosJogador: ipt_minutos.value
-    }
-}
-
-function CadStatsTimeOpp() {
-    statsJogador = {
-        jogador: null,
-        pontos: ipt_pontos.value,
-        rebotes: ipt_rebotes.value,
-        assistencias: ipt_assists.value,
-        tocos: ipt_tocos.value,
-        lancesLivresErrados: Number(ipt_ll_tentados.value - ipt_ll_certos.value),
-        lancesLivresCertos:ipt_ll_certos.value,
-        arremessosErrados: Number(ipt_arrms_tentados.value - ipt_arrms_certos.value),
-        arremessosCertos: ipt_arrms_certos.value,
-        minutosJogador: ipt_minutos.value
-    };
-};
-
 window.onscroll = function () {
     var nav = document.getElementById("navbar");
 
@@ -90,17 +38,5 @@ function closeNav() {
         }
 
         menuAtivo = false;
-    }
-}
-
-function showPassword() {
-    var inputsSenha = document.querySelectorAll("input[password]");
-    console.log(inputsSenha)
-    for(let i = 0; i < inputsSenha.length; i++) {
-        if(inputsSenha[i].type == "password") {
-            inputsSenha[i].type = "text";
-        } else {
-            inputsSenha[i].type = "password";
-        }
     }
 }
