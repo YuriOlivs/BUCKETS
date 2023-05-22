bfb864bf7671f6137fb999719091f7d14443f8f6
YuriOlivs <yuri.os.2004@outlook.com>
2023-05-22 07:10:26 -0300

    atualizando historico.html

--- site/public/js/historico.js
+++ site/public/js/historico.js
@@ -2,28 +2,33 @@ const containerHistorico = document.getElementById("container_historico");
 const slctStats = document.getElementById("slct_stats");
 
 function showUserStats() {
-    fetch('/stats/listar', {
-       method: 'GET',
-       heahders: {
-          "Content-type": "application/json"
-       }
-    }).then(function (resposta) {
-       if(resposta.ok) {
-          resposta.json().then(function (resposta) {
-             for (let i = 0; i < resposta.length; i++) {
-                var stat = resposta[i];
+   fetch('/stats/listar', {
+      method: 'GET',
+      headers: {
+         "Content-type": "application/json"
+      }
+   }).then(function (resposta) {
+      if(resposta.ok) {
+         resposta.json().then(function (resposta) {
+            for (let i = 0; i < resposta.length; i++) {
+               var stat = resposta[i];
 
-                var option = `<option value=""> ${stat.data_stat} </option>`
-                slctStats.innerHMTL += option;
-                console.log(stat.data_stat)
-             }
-          })
-       } else {
-          throw('Houve um erro na API');
-       }
-    })
- }
+               var option = `<option value=""> ${stat.data_stat} </option>`
+               slctStats.innerHTML += option;
+               console.log(stat.data_stat)
+            }
+         })
+      } else {
+         throw('Houve um erro na API');
+      }
+   })
+}
 
  window.onload = () => {
     showUserStats();
  }
+
+
+ //sua função
+ question[`question${G}`].classList.toggle(show);
+ //fim da sua função
